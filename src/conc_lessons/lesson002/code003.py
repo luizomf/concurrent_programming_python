4fda6e15d113637b65a4d49641b4ee2f0b17bd15
Luiz Otávio <luizomf@gmail.com>
2025-10-30 07:26:22 -0300

    add subscriber comment to remember later

--- src/conc_lessons/lesson002/code003.py
+++ src/conc_lessons/lesson002/code003.py
@@ -1,4 +1,7 @@
 # Criando minhas Threads por herança
+# @jairfsj -> Me passou nos comentários do vídeo anterior.
+# A pronúncia de "daemon" é dimon com do de í.
+# https://youtu.be/mDCJeoXvnhM?si=0quj4EFufqxh22gT
 
 if __name__ == "__main__":
     pass
