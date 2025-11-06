5ddfa1f62fb072349d88ad8c7c1324ade6281f63
Luiz Otávio <luizomf@gmail.com>
2025-11-06 12:38:48 -0300

    add topics for lesson

--- src/conc_lessons/lesson003/code007.py
+++ src/conc_lessons/lesson003/code007.py
@@ -1,3 +1,12 @@
 #
-# Title
+# Timer Thread em Python - Criando um timer estiloso
+#
+# - Limpar o código
+# - Trocar _seconds to _remaining
+# - Adicionar cores: current, stopped, paused e running (não esquecer RESET)
+# - Adicionar _content e property content para obter os valores da thread
+# - O método stop garante que não está pausado
+# - Criar método compose para criar a text box
+# - Criar métodos _set para running, stopped e paused
+# - Usar métodos próprios para stop, is running, pause, etc...
 #
